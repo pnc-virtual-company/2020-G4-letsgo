c1c70ddc98fc1e2a70118bafa3d906c1971add54
him123hey <heyhim99@gmail.com>
2020-07-19 10:38:07 +0700

    #30:remove category

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('categories','CategoryController');
+Route::get('removeCategory{id}', 'CategoryController@removeCategory')->name('remove');
 Route::get('/search','CategoryController@searchBar')->name('searchBar');
 Route::resource('events','EventController');
 Route::get('explore','EventController@showExploreEventView')->name('showExploreEventView');
