bd2c1409450a36d0088420d1ca84d604b1a3e30b
him123hey <heyhim99@gmail.com>
2020-07-23 13:21:09 +0700

    #51: your event view html

--- resources/views/Events/yourEvents.blade.php
+++ resources/views/Events/yourEvents.blade.php
@@ -2,6 +2,36 @@
 
 @section('content')
 <div class="container">
-    
+    <div class="row">
+        <div class="col-12">
+            <h2>Your Event</h2>
+            <a href="#" class="btn btn-primary float-right">CREATE</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 mt-5">
+            <div class="card mt-2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-2 mt-5">
+                            <h3></h3>
+                        </div>
+                        <div class="col-4 text-center mt-4">
+                            <h5></h5>
+                            <h3></h3>
+                            <p></p>
+                        </div>
+                        <div class="col-3">
+                            <img src="" alt="Not Found" class="img img-thumbnail m-3" style="width:150px; height:130px;">
+                        </div>
+                        <div class="col-3 mt-5">
+                            <a href="#" class="btn btn-warning float-right">EDIT</a>
+                            <a href="#" class="btn btn-danger float-right mr-3">CANCEL</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
