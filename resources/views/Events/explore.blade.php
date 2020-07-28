59729822b749c4e57916b9addf824e0f53cb4ed8
lysa-thorn <lysa.thorn@gmail.com>
2020-07-28 11:17:55 +0700

    #73: extend start date, location, format time in card

--- resources/views/Events/explore.blade.php
+++ resources/views/Events/explore.blade.php
@@ -36,21 +36,25 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-2 mt-5">
-                            @if($event->startTime < 12) <h3>{{$event->startTime}} AM</h3>
-                                @else
-                                <h3>{{$event->startTime}} PM</h3>
-                                @endif
+                            @if($event->startTime < 12)
+                                <h3>{{\Carbon\Carbon::createFromFormat('H:i:s',$event->startTime)->format('h:i')}} AM</h3>
+                            @else
+                                <h3>{{\Carbon\Carbon::createFromFormat('H:i:s',$event->startTime)->format('h:i')}} PM</h3>
+                            @endif
+                            <p>{{date('d-m-Y', strtotime($event->startTime))}}</p>
                         </div>
                         <div class="col-4 text-center mt-4">
                             <h5>{{$event->Category['name']}}</h5>
                             <h3>{{$event->title}}</h3>
                             <p>{{$event->numberOfMember}} Members going!</p>
+                            <p>{{$event->location}}</p>
                         </div>
-                        <div class="col-4">
+                        <div class="col-3">
                             <img src="{{asset('image/'.$event->eventPicture)}}" alt="Not Found" class="img img-thumbnail m-3" style="width:150px; height:130px;">
                         </div>
-                        <div class="col-2 mt-5">
-                            <a href="#" class="btn btn-light"><span class="material-icons float-left">check_circle_outline</span> Join</a>
+                        <div class="col-3 mt-5">
+                            <a href="#" class="btn btn-warning float-right">EDIT</a>
+                            <a class="delete btn btn-danger float-right mr-3" id="dlEvents" data-id="{{$event->id}}" data-target="#delete_event" data-toggle="modal" >CANCEL</a>
                         </div>
                     </div>
                 </div>
