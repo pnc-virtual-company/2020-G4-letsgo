5789d9327088282d23defd826c598f5254536acc
dalinStudent <hoemdalin@gmail.com>
2020-08-03 15:37:49 +0700

    #48:delete incorrect include

--- resources/views/Events/editEvent.blade.php
+++ resources/views/Events/editEvent.blade.php
@@ -1,79 +1,74 @@
 <!-- The Modal -->
 <div class="modal" id="editEventModal">
     <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-    <!-- Modal body -->
-    <div class="modal-body">
-    <div class="container">
-    <h5>Edit an event</h5>
+        <div class="modal-content">
+            <!-- Modal body -->
+            <div class="modal-body">
+                <div class="container">
+                    <h5>Edit an event</h5>
     
-    <form action="" method="post" id="updateEventModal" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
-    <div class="row">
-    <div class="col-8">
-    {{--======= Category =====--}}
-    <select id="cat" class="form-control" name="categoryid" required>
-    @foreach($categories as $category)
-    <option value={{$category->id}} required>{{$category->name}}</option>
-    @endforeach
-    </select>
-    {{--=======end Category =====--}}
-    <input id="title" type="text" required name="title" class="form-control mt-2">
-    <div class="row mt-2">
-    <div class="col-7">
-    <input id="startDate" type="date" required name="startDate" class="form-control">
-    </div>
-    <div class="col-5">
-    <input id="startTime" type="time" required name="startTime" class="form-control" >
-    </div>
-    </div>
-    <div class="row mt-2">
-    <div class="col-7">
-    <input type="date" required name="endDate" id="endDate" class="form-control" >
-    </div>
-    <div class="col-5">
-    <input type="time" required name="endTime" id="endTime" class="form-control">
-    </div>
-    </div>
+                    <form action="" method="post" id="updateEventModal" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                    <div class="row">
+                        <div class="col-8">
+                            {{--======= Category =====--}}
+                        <select id="cat" class="form-control" name="categoryid" required>
+                            @foreach($categories as $category)
+                                <option value={{$category->id}} required>{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                            {{--=======end Category =====--}}
+                            <input id="title" type="text" required name="title" class="form-control mt-2">
+                    <div class="row mt-2">
+                        <div class="col-7">
+                            <input id="startDate" type="date" required name="startDate" class="form-control">
+                        </div>
+                        <div class="col-5">
+                            <input id="startTime" type="time" required name="startTime" class="form-control" >
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-7">
+                            <input type="date" required name="endDate" id="endDate" class="form-control" >
+                    </div>
+                        <div class="col-5">
+                            <input type="time" required name="endTime" id="endTime" class="form-control">
+                        </div>
+                    </div>
     
-    <select id="city" name="location" required class="form-control mt-2">
-    @foreach($data as $item)
-    <option >{{$item['cityCountry']}}</option>
-    @endforeach
-    </select>
-    <textarea id="description" class="form-control mt-2" name="description" placeholder="Description"></textarea>
-    </div>
-    <div class="col-4">
-    <div class="img">
-    <img src="" id="img" alt="Not found" class="img-thumbnail">
-    {{-- <div class="text-center image-upload"> --}}
-    {{-- <label for="file-input">
-    <i class="material-icons m-2 text-primary">create</i>
-    </label> --}}
+                    <select id="city" name="location" required class="form-control mt-2">
+                        @foreach($data as $item)
+                            <option >{{$item['cityCountry']}}</option>
+                        @endforeach
+                    </select>
+                    <textarea id="description" class="form-control mt-2" name="description" placeholder="Description"></textarea>
+                </div>
+                <div class="col-4">
+                    <div class="img">
+                        <img src="" id="img" alt="Not found" class="img-thumbnail">
     
-    <input id="file-input" id="image-preview" class="col-12" type="file" name="eventPicture" />
+                            <input id="file-input" id="image-preview" class="col-12" type="file" name="eventPicture" />
     
     {{-- </div> --}}
-    </div>
-    </div>
-    </div>
-    <button type="submit" id="editEvent" class="btn text-primary float-right">SUBMIT</button>
-    <a href="#" data-dismiss="modal" class="btn float-right">DISCARD</a>
+                </div>
+            </div>
+        </div>
+                <button type="submit" id="editEvent" class="btn text-primary float-right">SUBMIT</button>
+                <a href="#" data-dismiss="modal" class="btn float-right">DISCARD</a>
     </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    
-    <script>
+<script>
     $(document).ready(function(){
+
     $('#editEventModal').on('show.bs.modal', function (event) {
     
     var button = $(event.relatedTarget) // Button that triggered the modal
-    console.log('hello')
     
     var category = button.data('category') // Extract info from data-* attributes
     
@@ -144,6 +139,6 @@
     
     
     })
-    });
+});
 </script>
     
